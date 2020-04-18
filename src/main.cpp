51402d2c636c83ddffe646732a432ad9c9f4f993
Daniel Eichhorn <dani.eichhorn@squix.ch>
2020-04-18 08:21:33 +0200

    Initial import

---
+++ src/main.cpp
@@ -0,0 +1,146 @@
+#include <Arduino.h>
+#include "settings.h"
+#include <WiFi.h>
+extern "C" {
+	#include "freertos/FreeRTOS.h"
+	#include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
+#include "settings.h"
+
+RTC_DATA_ATTR int bootCount = 0;
+
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
+
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+    Serial.printf("[WiFi-event] event: %d\n", event);
+    switch(event) {
+    case SYSTEM_EVENT_STA_GOT_IP:
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+        connectToMqtt();
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        Serial.println("WiFi lost connection");
+        xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+		    xTimerStart(wifiReconnectTimer, 0);
+        break;
+    }
+}
+
+
+
+void onMqttConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+
+  pinMode(REED_PIN, INPUT_PULLUP);
+  bool isOpen = digitalRead(REED_PIN);
+
+  String message = "{\"bootCount\": " + String(bootCount) 
+    + ", \"doorOpen\": " + String(isOpen ? "true" : "false") 
+    + ", \"millis\": " + String(millis()) + "}";
+  Serial.println(message);
+  uint16_t messageId = mqttClient.publish(MQTT_TOPIC, 2, false, message.c_str());
+  Serial.printf("Message id: %d\n", messageId);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+  Serial.printf("payload: %s\n", payload);
+  Serial.flush();
+
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.println("Going to sleep now");
+
+  // After the message has been sent to the MQTT broker 
+  // Prepare the device for sleep. The ESP32 should wake up, when
+  // the state of the door switch changes. Sadly the API doesn't offer
+  // a wake-up trigger on state change. It can only wake up when a certain
+  // state is monitored on the given PIN. So we are prepping the ESP32 to
+  // wake up on a state different from the current one.
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_15, !digitalRead(REED_PIN));
+  esp_deep_sleep_start();
+  Serial.println("This will never be printed");
+}
+
+void setup() {
+  Serial.begin(115200);
+  bootCount++;
+
+
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+
+  WiFi.onEvent(WiFiEvent);
+
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onSubscribe(onMqttSubscribe);
+  mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+
+  connectToWifi();
+
+}
+
+void loop() {
+  // not used
+}
---
+++ src/settings.h
@@ -0,0 +1,8 @@
+#define REED_PIN 15
+
+#define WIFI_SSID "yourssid"
+#define WIFI_PASSWORD "yourpassw0rd"
+
+#define MQTT_HOST IPAddress(192, 168, 0, 103)
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "ESPNOW/1"
